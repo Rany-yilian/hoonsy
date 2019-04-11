7aab9b3ef12095bfa7ec169abeb1d4c2cabf2f0e
yilian <873026940@qq.com>
2019-04-11 15:02:34 +0800

    four

--- http_server.c
+++ http_server.c
@@ -12,15 +12,24 @@
 #include <netinet/in.h>
 
 #define HTTP_MAX_LENGTH 1024
-struct Http_format{
+struct Http_format{//请求行
 	
 	char * type;
 	char * http_request_url;
 	char * http_version;
 };
+struct Node{//用链表结构进行存储
+	char * name;//请求头对应参数值的key
+	char * value;//请求头对应参数的值
+	struct Node *next;
+};
+char * request_head;//代表请求行
+struct Node *new_link,*head;
+
+
 struct Http_format http_format;
 int header_parse(char * buff,int len);
-
+void print_head(struct Node * head);
 int main(int argc, char *argv[])
 {
     int server_sockfd;
@@ -56,6 +65,9 @@ int main(int argc, char *argv[])
    
     for(;;)
     {
+		new_link = (struct Node *)malloc(sizeof(struct Node *));
+		new_link->next = NULL;
+		head= new_link;
         client_sockfd = accept(server_sockfd, (struct sockaddr *) &remote_addr, &sin_size);
         if (client_sockfd < 0)
             error("ERROR on accept");
@@ -71,12 +83,15 @@ int main(int argc, char *argv[])
             char html[] = "<!DOCTYPE html><html><head></head><body><h1>This is server_</h1></body></html>";
             recv(client_sockfd, buf, BUFSIZ, 0);
              header_parse(buf,strlen(buf));
-		printf("%s\n",http_format.type);
-		printf("%s\n",http_format.http_request_url);
-		printf("%s\n",http_format.http_version);
-               printf("%s\n", buf);
-		
 			
+               //printf("%s\n", buf);
+		
+			 printf("\n\n");
+			 printf("请求行:\n");
+			printf("%s\n",http_format.type);
+			printf("%s\n",http_format.http_request_url);
+			printf("%s\n",http_format.http_version);
+			print_head(head);
 			/*************由进程去连接本地apache服务*************/
 			int sockfd,numbers;
 		
@@ -101,7 +116,7 @@ int main(int argc, char *argv[])
 				//开始阻塞
 				numbers = recv(sockfd,buf,BUFSIZ,0);
 				buf[numbers]= '\0';
-				printf("received:%s\n",buf);
+				//printf("received:%s\n",buf);
 			
 			
 			close(sockfd);
@@ -121,6 +136,9 @@ int main(int argc, char *argv[])
     return 0;
 }
 
+/**
+解析请求头
+*/
 int header_parse(char * buff,int length){
 	char *stop , *stop1;
 	char *new_buff = buff;
@@ -152,15 +170,71 @@ int header_parse(char * buff,int length){
 		++new_buff;
 		++stop1;
 		if(sscanf(new_buff,"HTTP/%[0-9].%[0-9]",v1,v2)==2){
+			strcat(v3,"HTTP");
 			strcat(v3,v1);
 			strcat(v3,".");
 			strcat(v3,v2);
 			strcpy(http_format.http_version,v3);
+			//继续递增指针到换行
+			while(*(++new_buff)!='\r');//指向第一个回车符
+			++new_buff;//指向第一个换行符
+			++new_buff;//第二行
+			request_head = new_buff;
+			parse_header_request(request_head);
+		}else{
+			return -1;
 		}
 	}
 	
 }
 
+/**
+解析请求头
+*/
+int parse_header_request(char * buff){
+	char *index,*start,* line = buff;
+	struct Node * new_node;
+	char * value;
+	while((index = strstr(line,":"))!=NULL){
+		char * value = (char *)malloc(1500);
+		memcpy(value,line,index-line);
+		//if(!memcmp(value,'Host',4)){
+			new_node = (struct Node *)malloc(sizeof(struct Node *));
+			new_node->name = value;//得到第一参数键
+			++index;
+			++index;
+			start = index;//start指向第一个值的第一个字符
+			while(*(++index)!='\r');//指向第一个回车符
+			value = (char *)malloc(1500);
+			memcpy(value,start,index-start);
+			new_node->value = value;//得到第一参数值
+			new_node->next = NULL;
+			new_link->next = new_node;
+			new_link = new_node;
+			++index;//指向第一个换行符
+			++index;//重新一行的第一个字符
+			line = index;
+	
+		//}
+	}
+	
+}
+
+void print_head(struct Node * head){
+	struct Node * temp;
+	temp = head->next;
+	printf("请求头:\n");
+	while(temp){
+		printf("%s:",temp->name);
+		printf("%s",temp->value);
+		temp = temp->next;
+		printf("\n");
+	}
+	printf("请求头结束\n");
+}
+/*
+获取请求方式
+*/
 int get_head_type(char * buff){
 	http_format.type = (char*)malloc(100*sizeof(char));
 	if(!memcmp(buff,"GET",3)){
@@ -193,11 +267,11 @@ int get_head_type(char * buff){
 	else if(!memcmp(buff,"COPY",4)){
 		strcpy(http_format.type,"COPY");
 	}
-	else if(!memcmp(buff,"LINK",4)){
-		strcpy(http_format.type,"LINK");
+	else if(!memcmp(buff,"new_link",4)){
+		strcpy(http_format.type,"new_link");
 	}
-	else if(!memcmp(buff,"UNLINK",6)){
-		strcpy(http_format.type,"UNLINK");
+	else if(!memcmp(buff,"UNnew_link",6)){
+		strcpy(http_format.type,"UNnew_link");
 	}
 	else if(!memcmp(buff,"WRAPPED",7)){
 		strcpy(http_format.type,"WRAPPED");
@@ -206,6 +280,8 @@ int get_head_type(char * buff){
 		strcpy(http_format.type,"WRAPPED");
 	}
 }
+
+
 
 
 
